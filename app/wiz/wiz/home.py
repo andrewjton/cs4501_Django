f309224f21b3bd2ce60b47c454583223259af3ca
aton <at4my@virginia.ed>
2016-09-01 21:35:06 -0400

    finished debugging & cleaning project 1.

--- app/wiz/wiz/home.py
+++ app/wiz/wiz/home.py
@@ -2,15 +2,16 @@ from django.http import HttpResponse
 
 def index(request):
 	httpcode = """
+<!DOCTYPE HTML>
 <html>
-<head>
-	<link rel=\"stylesheet\" type=\"text/css\" href=\"/static/admin/css/base.css\"/>
-</head>
-<body>
-	<div id=\"container\"><div id=\"header\"><div id=\"branding\">
-		<h1>Hello World!</h1>
-	</div></div></div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>1st Django Project</title>
+    </head>
+    <body>
+            <h1 style="text-align: center; color: red; font-size: 72px;">Hello World!</h1>
+            <img src="http://andrewton.net/content/profPic.png" alt="andrew ton. for blind people">
+    </body>
 </html>
 """
 	return HttpResponse(httpcode)
