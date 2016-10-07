4861e332fc809cc6ab62f28dc61bdf5923f17eda
myw4pd <mdubz@d-172-27-42-99.dhcp.virginia.edu>
2016-10-07 16:18:48 -0400

    displaying jobs on home and item detail

--- frontend/home/views.py
+++ frontend/home/views.py
@@ -5,11 +5,18 @@ import json
 
 def index(request):
     return render(request, 'home/index.html', {})
+    response = requests.get('http://exp-api:8000/api/job/all')
+    jsonJobsList = json.loads(response.content.decode("utf8"))['resp']
+    return render(request, 'home/index.html', {'allJobs': jsonJobsList})
 
 def about(request):
     return render(request, 'home/about.html', {})
 
-def clean(request):
+def job(request, jobID):
+    return render(request, 'home/job.html', {})
     response = requests.get('http://exp-api:8000/api/job/all')
-    jsonJobsList = json.loads(response.content.decode())['resp']
-    return render(request, 'home/clean.html', {'allJobs': jsonJobsList})
+    jsonJobsList = json.loads(response.content.decode("utf8"))['resp']
+    #get specific job that has given jobID
+    return render(request, 'home/job.html', {'job': jsonJobsList})
+    
+    
