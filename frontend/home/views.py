2d0a444fd774afbace03d0b114166b6856b2aa78
Brandon Peck <brandonpeck13@gmail.com>
2016-10-07 22:41:45 -0400

    fixed front end debug

--- frontend/home/views.py
+++ frontend/home/views.py
@@ -8,16 +8,14 @@ def index(request):
     jsonJobsList = json.loads(response.content.decode("utf8"))['resp']
     #return HttpResponse(jsonJobsList)
 
-    return render(request, 'home/index.html', {'alljobs': jsonJobsList})
+    return render(request, 'home/index.html', {'allJobs': jsonJobsList})
 
 def about(request):
     return render(request, 'home/about.html', {})
 
 def job(request, jobID):
-    response = requests.get('http://exp-api:8000/api/v1/job/all/')
-    
-    jsonJobsList = json.loads(response.content.decode("utf8"))['resp']
-    #get specific job that has given jobID
+    response = requests.get('http://exp-api:8000/api/v1/job/'+str(jobID)+"/")
+    jsonJobsList = json.loads(response.content.decode("utf8"))['resp']    
     return render(request, 'home/job.html', {'job': jsonJobsList})
     
     
